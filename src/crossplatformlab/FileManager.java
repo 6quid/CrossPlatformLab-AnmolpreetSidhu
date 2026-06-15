4867229f53c9dccece0aaed4c69b9b497f08f757
squid <sidhu769@sheridancollege.ca>
2026-06-15 12:35:18 -0400

    Initial commit: FileManager and multi-os workflow

---
+++ src/crossplatformlab/FileManager.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package crossplatformlab;
+
+/**
+ *
+ * @author Anmolpreet Sidhu
+ */
+public class FileManager {
+    
+    public void printMe(){
+        System.out.println("This is FileManager Class");
+    }
+}
